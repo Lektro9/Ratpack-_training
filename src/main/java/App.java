76adbfc567d8d3a823639201a6b0dddfc3a25fe0
Lektro9 <lkroll@scoop-software.de>
2019-08-21 15:47:09 +0200

    add users/new for creating new users with json posts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import com.google.gson.Gson;
 import ratpack.exec.Promise;
 import ratpack.http.Request;
 import ratpack.http.Status;
+import ratpack.jackson.Jackson;
 import ratpack.parse.Parse;
 import ratpack.server.RatpackServer;
 import ratpack.util.MultiValueMap;
@@ -23,30 +24,41 @@ public class App {
         users.add(user1);
 
         RatpackServer.start(serverSpec -> serverSpec
-            .handlers(chain -> chain
-            .get( ctx -> {
-                MultiValueMap<String, String> nameMap = ctx.getRequest().getQueryParams();
-                String name = nameMap.get("name");
-                ctx.render("Hello " + name);})
-                    .path("foo", ctx ->
-                    ctx.byMethod( s -> s
-                            .get(() -> ctx.render("goo"))
-                            .post(() -> { ctx.render("success, but dont know how to grab content, yet");
-                            })
-                    ))
-                    .get("users", ctx ->
-                            ctx.byContent(m -> m
-                                    .html(() -> {
-                                        String content = "";
-                                        for (int i = 0; i < users.size(); i++){
-                                            content += "<p>" + users.get(i).name + "</p>";
-                                            content += "<p>" + users.get(i).email + "</p>";
-                                        }
-                                        ctx.render(content);
-                                    })
-                            .json(() -> ctx.render(json(users))
-                            ))
-            )
-        ));
+                .handlers(chain -> chain
+                        .get(ctx -> {
+                            MultiValueMap<String, String> nameMap = ctx.getRequest().getQueryParams();
+                            String name = nameMap.get("name");
+                            ctx.render("Hello " + name);
+                        })
+                        .path("foo", ctx ->
+                                ctx.byMethod(s -> s
+                                        .get(() -> ctx.render("goo"))
+                                        .post(() -> {
+                                            ctx.render("success, but dont know how to grab content, yet");
+                                        })
+                                ))
+                        .get("users", ctx ->
+                                ctx.byContent(m -> m
+                                        .html(() -> {
+                                            String content = "";
+                                            for (int i = 0; i < users.size(); i++) {
+                                                content += "<p>" + users.get(i).name + "</p>";
+                                                content += "<p>" + users.get(i).email + "</p>";
+                                            }
+                                            ctx.render(content);
+                                        })
+                                        .json(() -> ctx.render(json(users))
+                                        )
+                                )
+                        )
+                        .post("users/new", ctx -> {
+                            Promise<User> user = ctx.parse(Jackson.fromJson(User.class));
+                            user.then(u -> {
+                                users.add(u);
+                                ctx.render(u.name);
+                            });
+                        })
+                )
+        );
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,9 +1,13 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class User {
     public int id;
     public String name;
     public String email;
 
-    public User(int id, String name, String email) {
+    public User(@JsonProperty("id") int id,
+                @JsonProperty("name") String name,
+                @JsonProperty("email") String email) {
         this.id = id;
         this.name = name;
         this.email = email;
